d84a7bf9d6629eb9b550b413eb0546a648b74e75
zaneta989 <zaneta989@gmail.com>
2017-08-02 20:08:41 +0200

    install behat and example test

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,11 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\MinkExtension\Context\MinkContext;
+
+class FeatureContext extends MinkContext implements Context
+{
+    public function __construct()
+    {
+    }
+}
