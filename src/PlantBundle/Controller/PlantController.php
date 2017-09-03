2fdd967294cf38567799a1cc3a3cb1d19f94e9fd
zaneta989 <zaneta989@gmail.com>
2017-09-03 17:21:24 +0200

    user can modify his plant

--- src/PlantBundle/Controller/PlantController.php
+++ src/PlantBundle/Controller/PlantController.php
@@ -84,5 +84,66 @@ class PlantController extends Controller
             'form' => $form->createView(),
         ));
     }
+    /**
+     * Displays a form to edit an existing plant entity.
+     *
+     * @Route("/plant/{id}/edit", name="plantEdit")
+     */
+    public function editAction(Request $request,  $id)
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $plant= $em->getRepository('PlantBundle:Plant')->find($id);
+
+        if ($plant==null || $plant->getOwner()!=$user)
+        {
+            $this->addFlash('error', 'Could not find plant');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $form =  $this->createForm('PlantBundle\Form\PlantType', $plant, array(
+            'action' => $this->generateUrl('plantEdit', array('id'=>$id))
+        ));
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em ->persist($plant);
+            $em ->flush();
+            $this->addFlash('sucess', 'Plant chnged!');
+            return $this->redirectToRoute('showPlant', array('id'=>$plant->getId()));
+        }
+        return $this->render('plant/edit.html.twig', array(
+            'form' => $form->createView(),
+            'plant' => $plant,
+        ));
+    }
+    /**
+     * @Route("/plant/setWatered/{id}", name="plantSetWatered")
+     */
+    public function setTodayAction(Request $request, $id)
+    {
+        $user = $this->getUser();
+        $plant = $this->getDoctrine()
+            ->getRepository('PlantBundle:Plant')
+            ->find($id);
+        if($plant == null || $plant->getOwner() != $user)
+        {
+            $this->addFlash('error', 'Could not find plant');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($plant->moveToToday());
+        $em->flush();
+
+        $this->addFlash('sucess',  'The plant was watered today');
+
+        return $this->redirect( $request->headers->get('referer') );
+    }
 }
 
