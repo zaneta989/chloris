ead1a19bc9b3b881ae6ddc7716dde2e685d09d67
zaneta989 <zaneta989@gmail.com>
2017-10-07 16:27:18 +0200

    alt/title where appropriate

--- src/PlantBundle/Controller/PlantController.php
+++ src/PlantBundle/Controller/PlantController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class PlantController extends Controller
 {
     /**
-     * @Route("/plant/all", name="myPlantsTableView")
+     * @Route("/plant/all", name="myPlants")
      * @return Response
      */
     public function indexTableViewAction()
